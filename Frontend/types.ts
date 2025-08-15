36ea4299d161db6d0228da4c9d606eaf6c5a5ccb
SANJAY222-2006 <sanjay236002@gmail.com>
2025-08-15 23:38:04 +0530

    doneeee

--- Frontend/types.ts
+++ Frontend/types.ts
@@ -14,14 +14,3 @@ export interface WeatherData {
   };
 }
 // --- ADD THIS NEW INTERFACE ---
-// This defines the shape of a single item in the forecast list
-export interface ForecastItem {
-  dt: number;
-  main: {
-    temp_max: number;
-    temp_min: number;
-  };
-  weather: {
-    icon: string;
-  }[];
-}
